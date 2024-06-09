dbd765f6aebc12fbb061d0c033eda92ab2c3500f
Arcee69 <olawalearcee@gmail.com>
2024-06-09 14:50:10 +0100

    News Responsiveness

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -63,24 +63,24 @@ const News = () => {
 
   return (
     <div className='flex flex-col'>
-        <div className='bg-[#fff] px-[100px] py-[19px] flex items-center justify-between '>
+        <div className='bg-[#fff] px-[100px] py-[19px] hidden lg:flex items-center justify-between '>
             <div className='flex gap-1 items-center'>
                 <p className='text-[#00AA55] font-manja font-bold text-base '>National Orientation Agency</p>
                 <IoChevronForwardOutline className='mb-1'/>
                 <p className='text-[#222222] font-bold text-base font-manja'>News</p>
             </div>
         </div>
-        <div className='flex flex-col gap-1 px-[100px]'>
+        <div className='flex flex-col gap-1 px-5 mt-20 lg:mt-0 lg:px-[100px]'>
             <div className='flex flex-col gap-1'>
-                <p className='text-[#222222] font-mont_alt font-bold text-[40px]'>NOA NEWS</p>
+                <p className='text-[#222222] font-mont_alt font-bold text-[24px] lg:text-[40px]'>NOA NEWS</p>
                 <img src={Chain} alt='Chain' className='w-[54px] ' />
             </div>
 
-            <div className='flex items-center mt-[40px]'>
+            <div className='flex flex-col lg:flex-row gap-5  items-center mt-[40px]'>
                 {
                     data?.map((item, index) => (
                         <div key={index} className='flex flex-col '>
-                            <img src={item?.img} alt='Poster' className='w-[279px] h-[200px]' />
+                            <img src={item?.img} alt='Poster' className='lg:w-[279px] lg:h-[200px]' />
                             <div className='flex flex-col mt-4'>
                                 <p className='font-bold text-[#222222] '>{item?.title}</p>
                                 <div className='flex items-center gap-1.5'>
@@ -100,11 +100,11 @@ const News = () => {
                 }
             </div>
 
-            <div className='flex items-center mt-[40px]'>
+            <div className='flex flex-col lg:flex-row gap-5 items-center mt-[40px]'>
                 {
                     data?.map((item, index) => (
                         <div key={index} className='flex flex-col '>
-                            <img src={item?.img} alt='Poster' className='w-[279px] h-[200px]' />
+                            <img src={item?.img} alt='Poster' className='lg:w-[279px] lg:h-[200px]' />
                             <div className='flex flex-col mt-4'>
                                 <p className='font-bold text-[#222222] '>{item?.title}</p>
                                 <div className='flex items-center gap-1.5'>
