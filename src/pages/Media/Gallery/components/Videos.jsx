747bd4b76c75b714eb52d9f4e205696db06255ab
Arcee69 <olawalearcee@gmail.com>
2024-06-09 16:31:59 +0100

    Hot Fix

--- src/pages/Media/Gallery/components/Videos.jsx
+++ src/pages/Media/Gallery/components/Videos.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import ReactPlayer from 'react-player/youtube'
 
 import Chain from "../../../../assets/png/chain.png"
 
-import ReactPlayer from 'react-player/youtube'
-
 const Videos = () => {
+
   return (
     <div className='flex flex-col'>
       <div className='flex flex-col gap-1'>
@@ -12,21 +12,25 @@ const Videos = () => {
         <img src={Chain} alt='Chain' className='w-[54px] ' />
       </div>
 
-      <div className='mt-[40px] grid grid-cols-1 lg:grid-cols-2  items-center' style={{ gap: '1.25rem' }}>
+      <div className='mt-[40px] grid grid-cols-1 lg:grid-cols-3 gap-2 items-center '>
         <div className="w-[200px]">
-          <ReactPlayer url='https://youtu.be/fASeqCgHEu8' style={{ width: "150px", height:"150px" }} />
+          <ReactPlayer url='https://youtu.be/fASeqCgHEu8' width={350} />
+        </div>
+        
+        <div className="w-[200px]">
+          <ReactPlayer url='https://youtu.be/8T7pY9HXRJ4' width={350} />
         </div>
         <div className="w-[200px]">
-          <ReactPlayer url='https://youtu.be/8T7pY9HXRJ4' />
+          <ReactPlayer url='https://youtu.be/IcPTW3-yFYU'  width={350} />
         </div>
         <div className="w-[200px]">
-          <ReactPlayer url='https://youtu.be/IcPTW3-yFYU' />
+          <ReactPlayer url='https://youtu.be/rzyjksTw38k'  width={350} />
         </div>
         <div className="w-[200px]">
-          <ReactPlayer url='https://youtu.be/rzyjksTw38k' />
+          <ReactPlayer url='https://youtu.be/HOVX8i3IcNI' width={350}/>
         </div>
         <div className="w-[200px]">
-          <ReactPlayer url='https://youtu.be/HOVX8i3IcNI' />
+          <ReactPlayer url='https://youtu.be/1I7Y7U3yPFI' width={350}/>
         </div>
       </div>
 
