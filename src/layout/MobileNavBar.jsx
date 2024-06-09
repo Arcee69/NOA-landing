43c2c4331cc0af4161686e4e9417d650d1fe0af3
Arcee69 <olawalearcee@gmail.com>
2024-06-09 16:34:23 +0100

    Hot Fix

--- src/layout/MobileNavBar.jsx
+++ src/layout/MobileNavBar.jsx
@@ -5,13 +5,24 @@ import { CgSpinner } from 'react-icons/cg';
 
 import Logo from "../assets/svg/logo.svg"
 import Close from "../assets/svg/closeIcon.svg"
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 
 
 const MobileNavBar = ({ handleClose }) => {
     const [userName, setUserName] = useState("")
- 
+    const [openAgencyDropdown, setOpenAgencyDropdown] = useState(false)
+    const [openMediaDropdown, setOpenMediaDropdown] = useState(false)
 
     const navigate = useNavigate()
+
+    const handleAgency = () => {
+      setOpenAgencyDropdown(prev => !prev)
+    }
+
+    const handleMedia = () => {
+      setOpenMediaDropdown(prev => !prev)
+    }
+
 
 
 
@@ -25,11 +36,40 @@ const MobileNavBar = ({ handleClose }) => {
             </div>
             <div className="mt-[32px] flex flex-col gap-y-[24px] pb-[16px]">
                 <p onClick={() => {navigate("/"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">Home</p>
-                <p onClick={() => {navigate("/"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">The Agency</p>
-                <p onClick={() => {navigate("/"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">Activities</p>
-                <p onClick={() => {navigate("/"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">News</p>
-                <p onClick={() => {navigate("/"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">Media Center</p>
-                <p onClick={() => {navigate("/"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">Partnership</p>
+                <div className='flex flex-col gap-2'>
+                  <div className='flex items-center justify-between' onClick={() => handleAgency()}>
+                    <p className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">The Agency</p>
+                    {openAgencyDropdown ? <IoIosArrowUp className='text-[#ccc] text-[24px]'/> : <IoIosArrowDown className='text-[#ccc] text-[24px]' />}
+                  </div> 
+                  {
+                    openAgencyDropdown && (
+                      <div className='flex flex-col gap-3 mt-3 px-3'>
+                          <p onClick={() => {navigate("/about"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-sm  text-[#00141B]">About Us</p>
+                          <p onClick={() => {navigate("/management"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-sm  text-[#00141B]">Management</p>
+                          <p onClick={() => {navigate("/noa-past-executives"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-sm  text-[#00141B]">NOA Past Chief Executives</p>
+                          <p onClick={() => {navigate("/state-offices"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-sm  text-[#00141B]">State Office Addresses</p>
+                          <p onClick={() => {navigate("/foi"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-sm  text-[#00141B]">FOI Desk</p>
+                      </div>
+                    )
+                  }
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <div className='flex items-center justify-between' onClick={() => handleMedia()}>
+                    <p className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">Media Center</p>
+                    {openMediaDropdown ? <IoIosArrowUp className='text-[#ccc] text-[24px]'/> : <IoIosArrowDown className='text-[#ccc] text-[24px]' />}
+                  </div>
+                  {
+                    openMediaDropdown && (
+                      <div className='flex flex-col gap-3 mt-3 px-3'>
+                          <p onClick={() => {navigate("/content-factory"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-sm  text-[#00141B]">Content Factory</p>
+                          <p onClick={() => {navigate("/gallery"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-sm  text-[#00141B]">Gallery</p>
+                      </div>
+                    )
+                  } 
+                </div>
+                <p onClick={() => {navigate("/hackathon"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">Hackathon</p>
+                <p onClick={() => {navigate("/news"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">News</p>
+                <p onClick={() => {navigate("/contact"); window.scrollTo(0,0); handleClose()}} className="font-mont  cursor-pointer font-semibold text-[17px]  text-[#00141B]">Contact</p>
                
             </div>
           
