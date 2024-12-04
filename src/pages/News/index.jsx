6eff9e3b0acddf8ab5302480ea0c91c2d34b921c
Arcee69 <olawalearcee@gmail.com>
2024-12-04 21:38:54 +0100

    Hot Fix

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { IoChevronForwardOutline } from 'react-icons/io5'
 
-import PicA from "../../assets/png/picA.png"
-import PicB from "../../assets/png/picB.png"
-import PicC from "../../assets/png/picC.png"
-import PicD from "../../assets/png/picD.png"
-import User from "../../assets/png/user.png"
 import Chain from "../../assets/png/chain.png"
 import Suggestion from "../../assets/png/suggestion.png"
 
@@ -16,74 +11,48 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 
-
 const News = () => {
     const [newsData, setNewsData] = useState([])
     const [loading, setLoading] = useState(false)
+    const [prevPageUrl, setPrevPageUrl] = useState(null);
+    const [nextPageUrl, setNextPageUrl] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+  
 
     const navigate = useNavigate()
 
-    const fetchNews = async () => {
-        setLoading(true)
-        await axios.get("https://api.admin.noa.gov.ng/api/post")
-        .then((res) => {
-            console.log(res, "vibes")
-            setLoading(false)
-            setNewsData(res?.data?.data?.posts)
-        })
-        .catch((err) => {
-            setLoading(false)
-            console.log(err, "err")
-        })
+    
+  const fetchNews = async (url = "https://api.admin.noa.gov.ng/api/post") => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      console.log(res, "addict")
+      const data = res.data;
+
+      setNewsData(data?.data || []);
+      setPrevPageUrl(data.pagination?.prev_page_url);
+      setNextPageUrl(data.pagination?.next_page_url);
+      setCurrentPage(data.pagination?.current_page);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        fetchNews()
-    }, [])
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  const handlePrevPage = () => {
+    if (prevPageUrl) fetchNews(prevPageUrl);
+  };
+
+  const handleNextPage = () => {
+    if (nextPageUrl) fetchNews(nextPageUrl);
+  };
 
 
-    // const data = [
-    //     {
-    //         id: 1,
-    //         img: PicA,
-    //         dept: "HEADQUARTERS",
-    //         name: "Deji Oyeleke",
-    //         date: "08.05.2024",
-    //         views: "23 views",
-    //         comment: "4 comments",
-    //         title: "Pressing for Change: Ministries Unite to Address Environmental Crises"
-    //     },
-    //     {
-    //         id: 2,
-    //         img: PicB,
-    //         dept: "HEADQUARTERS",
-    //         name: "Deji Oyeleke",
-    //         date: "08.05.2024",
-    //         views: "23 views",
-    //         comment: "4 comments",
-    //         title: "Pressing for Change: Ministries Unite to Address Environmental Crises"
-    //     },
-    //     {
-    //         id: 3,
-    //         img: PicC,
-    //         dept: "HEADQUARTERS",
-    //         name: "Deji Oyeleke",
-    //         date: "08.05.2024",
-    //         views: "23 views",
-    //         comment: "4 comments",
-    //         title: "Pressing for Change: Ministries Unite to Address Environmental Crises"
-    //     },
-    //     {
-    //         id: 4,
-    //         img: PicD,
-    //         dept: "HEADQUARTERS",
-    //         name: "Deji Oyeleke",
-    //         date: "08.05.2024",
-    //         views: "23 views",
-    //         comment: "4 comments",
-    //         title: "Pressing for Change: Ministries Unite to Address Environmental Crises"
-    //     },
-    // ]
 
   return (
     <div className='flex flex-col'>
@@ -131,43 +100,64 @@ const News = () => {
                 </div>
             }
 
+            <div className="flex justify-center items-center gap-4 mt-10">
+                <button
+                    onClick={handlePrevPage}
+                    disabled={!prevPageUrl}
+                    className={`px-4 py-2 bg-[#00AA55] text-white font-bold rounded ${
+                    !prevPageUrl && "opacity-50 cursor-not-allowed"
+                    }`}
+                >
+                    Previous
+                </button>
+                <p className="text-[#222222] font-bold">Page {currentPage}</p>
+                <button
+                    onClick={handleNextPage}
+                    disabled={!nextPageUrl}
+                    className={`px-4 py-2 bg-[#00AA55] text-white font-bold rounded ${
+                    !nextPageUrl && "opacity-50 cursor-not-allowed"
+                    }`}
+                >
+                    Next
+                </button>
+            </div>
+
         </div>
 
         <div
-        style={{ background: `url(${Suggestion})`, backgroundSize: "cover", backgroundRepeat: "no-repeat"  }}
-        className='lg:h-[512px] w-full flex py-10 lg:py-0 items-center gap-[20px] lg:gap-[45px] justify-center flex-col mt-[100px]'
-    >
-        <p className='font-manja font-bold text-[#222222] text-[27px]'>Suggestion and Complaints</p>
-        <p className='text-center font-medium font-mont_alt text-[11px] text-[#757575] w-[337px]'>
-            Don't hesitate to reach us because we believe that citizens 
-            should participate in driving change and development in Nigeria.
-        </p>
+            style={{ background: `url(${Suggestion})`, backgroundSize: "cover", backgroundRepeat: "no-repeat"  }}
+            className='lg:h-[512px] w-full flex py-10 lg:py-0 items-center gap-[20px] lg:gap-[45px] justify-center flex-col mt-[100px]'
+        >
+            <p className='font-manja font-bold text-[#222222] text-[27px]'>Suggestion and Complaints</p>
+            <p className='text-center font-medium font-mont_alt text-[11px] text-[#757575] w-[337px]'>
+                Don't hesitate to reach us because we believe that citizens 
+                should participate in driving change and development in Nigeria.
+            </p>
 
-        <div className='flex flex-col lg:flex-row items-center gap-6'>
-            <div className='bg-[#fff] w-[127px] h-[150px] flex flex-col gap-5 items-center justify-center'>
-                <img src={CallB} alt='CallB' className='w-[31px] h-[31px]'/>
-                <div className='flex flex-col items-center gap-[5px]'>
-                    <p className='font-manja text-[#222222] font-bold text-[13px]'>Call on</p>
-                    <p className='text-[#757575] font-manja text-[11px]'>0703 738 3444</p>
+            <div className='flex flex-col lg:flex-row items-center gap-6'>
+                <div className='bg-[#fff] w-[127px] h-[150px] flex flex-col gap-5 items-center justify-center'>
+                    <img src={CallB} alt='CallB' className='w-[31px] h-[31px]'/>
+                    <div className='flex flex-col items-center gap-[5px]'>
+                        <p className='font-manja text-[#222222] font-bold text-[13px]'>Call on</p>
+                        <p className='text-[#757575] font-manja text-[11px]'>0703 738 3444</p>
+                    </div>
                 </div>
-            </div>
-            <div className='bg-[#fff] w-[127px] h-[150px] flex flex-col gap-5 items-center justify-center'>
-                <img src={Mail} alt='Mail' className='w-[31px] h-[31px]'/>
-                <div className='flex flex-col items-center gap-[5px]'>
-                    <p className='font-manja text-[#222222] font-bold text-[13px]'>Mail at</p>
-                    <p className='text-[#757575] font-manja text-[11px]'>admin@noa.gov.ng</p>
+                <div className='bg-[#fff] w-[127px] h-[150px] flex flex-col gap-5 items-center justify-center'>
+                    <img src={Mail} alt='Mail' className='w-[31px] h-[31px]'/>
+                    <div className='flex flex-col items-center gap-[5px]'>
+                        <p className='font-manja text-[#222222] font-bold text-[13px]'>Mail at</p>
+                        <p className='text-[#757575] font-manja text-[11px]'>admin@noa.gov.ng</p>
+                    </div>
                 </div>
-            </div>
-            <div className='bg-[#fff] w-[127px] h-[150px] flex flex-col gap-5 items-center justify-center'>
-                <img src={Time} alt='Time' className='w-[31px] h-[31px]'/>
-                <div className='flex flex-col items-center gap-[5px]'>
-                    <p className='font-manja text-[#222222] font-bold text-[13px]'>Open hrs</p>
-                    <p className='text-[#757575] font-manja text-[11px]'>8am to 4pm</p>
+                <div className='bg-[#fff] w-[127px] h-[150px] flex flex-col gap-5 items-center justify-center'>
+                    <img src={Time} alt='Time' className='w-[31px] h-[31px]'/>
+                    <div className='flex flex-col items-center gap-[5px]'>
+                        <p className='font-manja text-[#222222] font-bold text-[13px]'>Open hrs</p>
+                        <p className='text-[#757575] font-manja text-[11px]'>8am to 4pm</p>
+                    </div>
                 </div>
-            </div>
 
-        </div>
-
+            </div>
 
         </div>
 
